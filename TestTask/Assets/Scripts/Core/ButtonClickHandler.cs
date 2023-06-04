1aa011c93c5c008ae68e5569f14c8eea0c548460
CrazyDream1 <35272226+CrazyDream1@users.noreply.github.com>
2023-06-04 14:31:38 +0300

    Add PlayMaker

---
+++ TestTask/Assets/Scripts/Core/ButtonClickHandler.cs
@@ -0,0 +1,21 @@
+﻿using HutongGames.PlayMaker;
+using UnityEngine;
+
+namespace Core
+{
+    [RequireComponent(typeof(PlayMakerFSM))]
+    public class ButtonClickHandler : MonoBehaviour
+    {
+        private PlayMakerFSM _fsm;
+
+        private void Awake()
+        {
+            _fsm = GetComponent<PlayMakerFSM>();
+        }
+
+        public void OnButtonClick()
+        {
+            _fsm.SendEvent("ButtonClickedEvent");
+        }
+    }
+}
